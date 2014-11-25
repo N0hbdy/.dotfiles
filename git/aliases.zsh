4103a7a9f27726204e3e9bb07f6b8075f9e8c90a
N0hbdy <austen.mcrae@gmail.com>
2014-11-25 11:34:21 -0800

    Updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,4 +17,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gcl='git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d'
+alias gcl='git branch --merged develop | grep -v "develop" | grep -v "master" | xargs -n 1 git branch -d'
--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,5 @@
 export EDITOR='vim'
+if [ -e "$HOME/.aws_source" ]
+then
+    source $HOME/.aws_source
+fi
